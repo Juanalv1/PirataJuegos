ba37ed342ffbcd6cad298f58a76837e4ff6394b9
Juan <elquesififa@gmail.com>
2023-10-06 13:46:12 -0500

    fized carousel

--- src/app/components/Carrusel.js
+++ src/app/components/Carrusel.js
@@ -1,30 +1,14 @@
 "use client"
 import { Carousel } from "flowbite-react";
+import Image from "next/image";
  
 export default function Carrusel({images}) {
   console.log(images)
   return (
-    <Carousel>
-    <img
-      alt="..."
-      src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-    />
-    <img
-      alt="..."
-      src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-    />
-    <img
-      alt="..."
-      src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-    />
-    <img
-      alt="..."
-      src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-    />
-    <img
-      alt="..."
-      src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-    />
+    <Carousel className="relative">
+      {images.map((image, index) => (
+        <Image src={image} key={index} fill={true} objectFit="contain"/>
+      ))}
   </Carousel>
   );
 }

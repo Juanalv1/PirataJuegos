08ad4ecc2d096c97bd736a1ee43d53e4ad936e38
Juan <elquesififa@gmail.com>
2023-10-06 09:37:28 -0500

    a

--- src/app/components/Carrusel.js
+++ src/app/components/Carrusel.js
@@ -1,4 +1,4 @@
-
+import Image from "next/image";
 import { Carousel, IconButton } from "./Carousel";
  
 export default function Carrusel({images}) {
@@ -8,10 +8,10 @@ export default function Carrusel({images}) {
       {/* {images.map((imagen, index) => (
         <img key={index} src={imagen} className="h-full w-full object-cover flex " alt={`imagen ${index}`}/>
       ))} */}
-       <img
+       <Image
         src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
         alt="image 1"
-        className="h-full w-full object-cover"
+        className="h-full w-full object-cover " width={100} height={100}
       />
     </Carousel>
   );

330f0f144b1f8e095c0eefcca8441fbeadec79e1
Juan <elquesififa@gmail.com>
2023-10-06 10:28:47 -0500

    asdas

--- src/app/components/Carrusel.js
+++ src/app/components/Carrusel.js
@@ -5,6 +5,8 @@ import { Carousel, IconButton } from "./Carousel";
 export default function Carrusel({images}) {
   console.log(images)
   return (
-    <Carousel className="" ></Carousel>
+    <div>
+      <h1 className="text-4xl font-bold">Carrusel</h1>
+    </div>
   );
 }

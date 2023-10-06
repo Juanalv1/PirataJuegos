4bbca081abb96de6e20166bcf06a156b42e3a8d3
Juan <elquesififa@gmail.com>
2023-10-06 10:32:23 -0500

    a

--- src/app/components/Carousel.js
+++
@@ -1,4 +0,0 @@
-"use client"
-import { Carousel, IconButton } from "@material-tailwind/react";
-
-export { Carousel, IconButton };
--- src/app/components/Carrusel.js
+++ src/app/components/Carrusel.js
@@ -1,6 +1,5 @@
 "use client"
-import Image from "next/image";
-import { Carousel, IconButton } from "./Carousel";
+
  
 export default function Carrusel({images}) {
   console.log(images)

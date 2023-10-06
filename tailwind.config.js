7fd43b751ab9bdf8d20d054f71503ccb6af8518b
Juan <elquesififa@gmail.com>
2023-10-06 09:22:26 -0500

    asdas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
+const withMT = require("@material-tailwind/utils/withMT");
 module.exports = withMT ({
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    './src/components/**/*/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@material-tailwind/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {

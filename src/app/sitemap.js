385dd97c4b3c3502b719d7a304b7c5622d10d404
Juan <elquesififa@gmail.com>
2023-10-30 22:54:37 -0500

    test2

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,28 +1,42 @@
 export default async function sitemap() {
   const fetchURL = 'http://piratajuegos.com/api/posts';
-  const site_URL = 'http://piratajuegos.com'
+  const site_URL = 'http://piratajuegos.com';
   const currentDate = new Date();
-  const req = await fetch(fetchURL)
-  const data = await req.json()
-  const titulo = data.post_title.replace(/ /g, "-")
 
+  try {
+    const req = await fetch(fetchURL);
+    const data = await req.json();
 
-  // Crear un array de objetos que representan las páginas/posts
-  const pages = posts.map((post) => ({
-    url: `${site_URL}/juegos/${titulo}`, // Codifica el título
-    lastModified: currentDate,
-    changeFrequency: 'monthly',
-    priority: 1,
-  }));
-  const staticPages = [
-    {
-      url: 'https://piratajuegos.com',
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 1,
-    },]
+    if (data && data.post_title) {
+      const titulo = data.post_title.replace(/ /g, "-");
 
-    const allPages = [...pages, ...staticPages];
+      // Resto del código para crear las páginas del sitemap
 
-    return allPages;
+      const pages = posts.map((post) => ({
+        url: `${site_URL}/juegos/${titulo}`,
+        lastModified: currentDate,
+        changeFrequency: 'monthly',
+        priority: 1,
+      }));
+      
+      const staticPages = [
+        {
+          url: 'https://piratajuegos.com',
+          lastModified: currentDate,
+          changeFrequency: 'monthly',
+          priority: 1,
+        },
+      ];
+
+      const allPages = [...pages, ...staticPages];
+
+      return allPages;
+    } else {
+      console.error('La propiedad "post_title" no existe en los datos.');
+      return [];
+    }
+  } catch (error) {
+    console.error('Error al obtener datos: ', error);
+    return [];
+  }
 }

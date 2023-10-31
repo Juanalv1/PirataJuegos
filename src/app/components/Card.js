47769b386f8f05e194dcecac8925e3c4e1be90a2
Juan <elquesififa@gmail.com>
2023-10-31 00:08:16 -0500

    seo enchanced

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,13 +1,15 @@
 import Link from "next/link"
 import CategoryBubble from "./CategoryBubble"
+import Image from "next/image";
+
 
 export default function Card ({titulo, categorias, version, image}) {
   const linkTitulo = titulo.replace(/ /g, "-");
   return (
-    <div className="bg-[#FFC93C] m-2  cursor-pointer w-56 flex flex-col shadow rounded-lg  border-black border-2 h-60 font-Quato flex-wrap">
+    <article className="bg-[#FFC93C] m-2  cursor-pointer w-56 flex flex-col shadow rounded-lg  border-black border-2 h-60 font-Quato flex-wrap">
       <Link href={`/juegos/${linkTitulo}`} className="w-full h-full">
         <div>
-          <img src={image} className="rounded-t-lg shadow h-28 w-full" alt={titulo}/>
+          <Image src={image} className="rounded-t-lg shadow h-28 w-full" alt={titulo} fill={true}/>
         </div>
        <div className="w-full flex flex-col p-2  justify-between h-[50%] ">
             <h1 className="font-semibold text-lg">{titulo}  {version}</h1>
@@ -18,6 +20,6 @@ export default function Card ({titulo, categorias, version, image}) {
             </div>
           </div>
       </Link>
-    </div>
+    </article>
   )
 }
--- src/app/components/SearchCard.js
+++ src/app/components/SearchCard.js
@@ -10,8 +10,8 @@ export default function SearchCard({image, title}) {
         <div className='w-[60%] h-[90%]'>
           <h3 className='font-semibold text-sm'>{title}</h3>
         </div>
-        <div className='w-[40%] h-[90%] flex justify-center items-center'>
-          <img src={image} className='w-full object-cover h-full ' alt={title}/>
+        <div className='w-[40%] h-[90%] flex justify-center items-center relative'>
+          <Image src={image} className='w-full object-cover h-full ' alt={title} fill={true}/>
         </div>
       </div>
     </Link>

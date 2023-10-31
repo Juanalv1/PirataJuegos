a0e475b9b51c8e1418c2805b5159ef2d15eeb175
Juan <elquesififa@gmail.com>
2023-10-31 17:20:20 -0500

    Changed the image source from srcImg to srcImg[0] (generated by useblackbox.ai )

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -10,7 +10,7 @@ export default function Card ({titulo, categorias, version, image}) {
     <article className="bg-[#FFC93C] m-2  cursor-pointer w-56 flex flex-col shadow rounded-lg  border-black border-2 h-60 font-Quato flex-wrap relative">
       <Link href={`/juegos/${linkTitulo}`} className="w-full h-full">
         <div className="relative w-full h-1/2">
-          <Image src={srcImg} className="rounded-t-lg shadow" alt={titulo} fill={true}/>
+          <Image src={srcImg[0]} className="rounded-t-lg shadow" alt={titulo} fill={true}/>
         </div>
        <div className="w-full flex flex-col p-2  justify-between h-[50%] ">
             <h1 className="font-semibold text-lg">{titulo}  {version}</h1>

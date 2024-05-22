2c4f44629320cba5ba96a0fa6dfa3ef2dded7e56
Juan <elquesififa@gmail.com>
2024-05-22 11:28:33 -0400

    a

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,36 +1,48 @@
-
-
-export default async function sitemap() {
+export default async function sitemap(req, res) {
   const site_URL = 'https://piratajuegos.com';
-  const currentDate = new Date();
+  const currentDate = new Date().toISOString();
 
   try {
-    const req = await fetch(`${process.env.NEXT_PUBLIC_FETCH_URL}/api/v1/posts`);
-    const posts = await req.json();
+    const response = await fetch(`${process.env.NEXT_PUBLIC_FETCH_URL}/api/v1/posts`);
+    const posts = await response.json();
 
-      // Resto del código para crear las páginas del sitemap
-      const pages = posts.map((post) => (
-        {
-        url: `${site_URL}/juegos/${post.post_title.replace(/ /g, "-")}`,
+    // Crear las páginas del sitemap
+    const pages = posts.map((post) => ({
+      url: `${site_URL}/juegos/${post.post_title.replace(/ /g, "-").replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;")}`,
+      lastModified: currentDate,
+      changeFrequency: 'monthly',
+      priority: 1,
+    }));
+
+    const staticPages = [
+      {
+        url: 'https://piratajuegos.com',
         lastModified: currentDate,
         changeFrequency: 'monthly',
         priority: 1,
-      }));
-      
-      const staticPages = [
-        {
-          url: 'https://piratajuegos.com',
-          lastModified: currentDate,
-          changeFrequency: 'monthly',
-          priority: 1,
-        },
-      ];
+      },
+    ];
 
-      const allPages = [...pages, ...staticPages];
+    const allPages = [...pages, ...staticPages];
 
-      return allPages;
+    // Crear el XML
+    const sitemapXML = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${allPages.map(page => `
+    <url>
+      <loc>${page.url}</loc>
+      <lastmod>${page.lastModified}</lastmod>
+      <changefreq>${page.changeFrequency}</changefreq>
+      <priority>${page.priority}</priority>
+    </url>
+  `).join('')}
+</urlset>`;
+
+    // Configurar el encabezado y enviar la respuesta
+    res.setHeader('Content-Type', 'application/xml');
+    res.status(200).send(sitemapXML);
   } catch (error) {
     console.error('Error al obtener datos: ', error);
-    return [];
+    res.status(500).send('Error al generar el sitemap');
   }
 }

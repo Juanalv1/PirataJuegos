138c2997c777e92333fc7a1ecd33b34f2f7d708f
Juan <elquesififa@gmail.com>
2024-05-22 11:41:54 -0400

    A

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,38 +1,44 @@
-
-
 export default async function sitemap() {
   const site_URL = 'https://piratajuegos.com';
-  const currentDate = new Date();
+  const currentDate = new Date().toISOString();
 
   try {
-    const req = await fetch(`${process.env.NEXT_PUBLIC_FETCH_URL}/api/v1/posts`, {next: {
-      revalidate: 3600
-    }});
+    const req = await fetch(`${process.env.NEXT_PUBLIC_FETCH_URL}/api/v1/posts`);
     const posts = await req.json();
 
-      // Resto del código para crear las páginas del sitemap
-      const pages = posts.map((post) => (
-        {
-        url: `${site_URL}/juegos/${post.post_title.replace(/ /g, "-")}`,
+    const escapeXml = (unsafe) => {
+      return unsafe.replace(/[<>&'"]/g, (c) => {
+        switch (c) {
+          case '<': return '&lt;';
+          case '>': return '&gt;';
+          case '&': return '&amp;';
+          case '"': return '&quot;';
+          case '\'': return '&apos;';
+        }
+      });
+    };
+
+    const pages = posts.map((post) => ({
+      url: `${site_URL}/juegos/${escapeXml(post.post_title.replace(/ /g, "-"))}`,
+      lastModified: currentDate,
+      changeFrequency: 'monthly',
+      priority: 1,
+    }));
+
+    const staticPages = [
+      {
+        url: 'https://piratajuegos.com',
         lastModified: currentDate,
         changeFrequency: 'monthly',
         priority: 1,
-      }));
-      
-      const staticPages = [
-        {
-          url: 'https://piratajuegos.com',
-          lastModified: currentDate,
-          changeFrequency: 'monthly',
-          priority: 1,
-        },
-      ];
+      },
+    ];
 
-      const allPages = [...pages, ...staticPages];
+    const allPages = [...pages, ...staticPages];
 
-      return allPages;
+    return allPages;
   } catch (error) {
     console.error('Error al obtener datos: ', error);
-    return [];
+    return '';
   }
 }
